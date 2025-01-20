ed034e9a846e6ccf85304fe63385ce6d00b6f121
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-20 15:20:01 +0700

    add code on about.py

--- about.py
+++ about.py
@@ -1,10 +1,20 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Multi-Page Streamlit App",
+    page_title="milhamat",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
-st.title("Main Application")
-st.write("Welcome to the main application! Use the sidebar to navigate to other pages.")
+st.title("About.")
+# st.write("Welcome to the main application! Use the sidebar to navigate to other pages.")
+
+col1, col2 = st.columns([1, 4])
+
+with col1:
+    st.image("artifacts/photo.png", width=200)
+
+with col2:
+    st.write("Hai my name is Ilham. I'm an Machine Learning Anthusiast, and this project is a part of my journey @Dibimbing Bootcamp.")
+    st.write("You can check my profile on [LinkedIn](linkedin.com/in/milhamat)")
+    st.write("You can check my full portofolion on [milhamat](https://milhamat.github.io/about.html)")
