2a76fc494ee7bf8886c0f6b7f79c3bbd4fe216dc
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-20 21:51:41 +0700

    add bulet point

--- pages/1_Analytics.py
+++ pages/1_Analytics.py
@@ -33,8 +33,8 @@ sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
 plt.title("Correlation Matrix")
 st.pyplot(fig1)
 st.subheader("Observations:")
-st.write("The correlation matrix shows the relationship between the features and the target variable SalePrice.")
-st.write("We can see that there is several features that strong correlation like LogSalePrice, OverallQual, GrLivArea, FullBath, TotRmsAbvGrd, YearBuilt, YearRemodAdd.")
+st.write("- The correlation matrix shows the relationship between the features and the target variable SalePrice.")
+st.write("- We can see that there is several features that strong correlation like LogSalePrice, OverallQual, GrLivArea, FullBath, TotRmsAbvGrd, YearBuilt, YearRemodAdd.")
 
 # Distribution Analysis: SalePrice
 st.header("Distribution Analysis: SalePrice")
@@ -45,7 +45,7 @@ plt.xlabel("SalePrice")
 plt.ylabel("Frequency")
 st.pyplot(fig2)
 st.subheader("Observations:")
-st.write("The histogram shows the distribution of SalePrice. We can see that the distribution is right-skewed, which can be addressed by log-transforming the target variable.")
+st.write("- The histogram shows the distribution of SalePrice. We can see that the distribution is right-skewed, which can be addressed by log-transforming the target variable.")
 
 # Feature vs SalePrice: Scatter Plots
 st.header("Feature vs SalePrice: Scatter Plots")
@@ -60,9 +60,9 @@ for i, feature in enumerate(features_to_plot, 1):
 fig3.tight_layout()
 st.pyplot(fig3)
 st.subheader("Observations:")
-st.write("The scatter plots show the relationship between the selected features and SalePrice. We can observe some patterns and outliers in the data.")
-st.write("Features like GrLivArea have a strong influence on SalePrice, while LotFrontage and LotArea show weaker correlations.")
-st.write("Houses built more recently generally have higher sale prices, though the relationship is not linear.")
+st.write("- The scatter plots show the relationship between the selected features and SalePrice. We can observe some patterns and outliers in the data.")
+st.write("- Features like GrLivArea have a strong influence on SalePrice, while LotFrontage and LotArea show weaker correlations.")
+st.write("- Houses built more recently generally have higher sale prices, though the relationship is not linear.")
 
 
 # Feature Group Analysis: OverallQual
@@ -74,7 +74,7 @@ plt.xlabel("Overall Quality")
 plt.ylabel("SalePrice")
 st.pyplot(fig4)
 st.subheader("Observations:")
-st.write("The boxplot shows the relationship between OverallQual (Overall Quality) and SalePrice. We can see that there is a clear positive correlation between the two variables.")
+st.write("- The boxplot shows the relationship between OverallQual (Overall Quality) and SalePrice. We can see that there is a clear positive correlation between the two variables.")
 
 # Time Trends: YearBuilt and YearRemodAdd
 st.header("Time Trends: YearBuilt and YearRemodAdd")
@@ -87,5 +87,5 @@ plt.ylabel("Average SalePrice")
 plt.legend()
 st.pyplot(fig5)
 st.subheader("Observations:")
-st.write("The line plot shows the average SalePrice trends over time based on the YearBuilt and YearRemodAdd features. We can observe dynamic fluctuations in SalePrice over the years and it has positive trends over the years.")
+st.write("- The line plot shows the average SalePrice trends over time based on the YearBuilt and YearRemodAdd features. We can observe dynamic fluctuations in SalePrice over the years and it has positive trends over the years.")
 
