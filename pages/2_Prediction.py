4fa6b9dbefafe459bae2db1aab36b7e45cabd0ec
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-20 16:46:20 +0700

    add model.pkl

--- pages/2_Prediction.py
+++ pages/2_Prediction.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import os
 
 st.set_page_config(page_title="Model Prediction", page_icon="🏠", layout="wide")
 st.title("Model Prediction")
+
+path = os.path.join("artifacts", "models", "model.pkl")
+
+st.write(path)
