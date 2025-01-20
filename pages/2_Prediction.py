dc83b0b8a6534bfbaf0bfd8e0a39b483738b96d0
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-20 20:14:32 +0700

    two column predict

--- pages/2_Prediction.py
+++ pages/2_Prediction.py
@@ -23,31 +23,39 @@ if 'predicted_price' not in st.session_state:
 st.set_page_config(page_title="Model Prediction", page_icon="🏠", layout="wide")
 st.title("Model Prediction")
 
-# Sliders for input features
-lf = st.slider("LotFrontage", 0, 130, 25)
-la = st.slider("LotArea", 0, 130, 25)
-oq = st.slider("OverallQual", 0, 130, 25)
-yb = st.slider("YearBuilt", 1900, 2023, 2000)
-yr = st.slider("YearRemodAdd", 1900, 2023, 2005)
-gla = st.slider("GrLivArea", 0, 130, 25)
-fb = st.slider("FullBath", 0, 5, 2)
-hb = st.slider("HalfBath", 0, 5, 1)
-bag = st.slider("BedroomAbvGr", 0, 10, 3)
-trag = st.slider("TotRmsAbvGrd", 0, 15, 7)
+# Two-column layout
+col1, col2 = st.columns([2, 1])
 
-# Input features array
-input_features = np.array([[lf, la, oq, yb, yr, gla, fb, hb, bag, trag]])
+# Input sliders in col1
+with col1:
+    st.header("Input Features")
+    lf = st.slider("LotFrontage", 0, 130, 25)
+    la = st.slider("LotArea", 0, 130, 25)
+    oq = st.slider("OverallQual", 0, 130, 25)
+    yb = st.slider("YearBuilt", 1900, 2023, 2000)
+    yr = st.slider("YearRemodAdd", 1900, 2023, 2005)
+    gla = st.slider("GrLivArea", 0, 130, 25)
+    fb = st.slider("FullBath", 0, 5, 2)
+    hb = st.slider("HalfBath", 0, 5, 1)
+    bag = st.slider("BedroomAbvGr", 0, 10, 3)
+    trag = st.slider("TotRmsAbvGrd", 0, 15, 7)
 
-# Button to trigger prediction
-if st.button("Predict Price"):
-    try:
-        # Perform prediction
-        predicted_price = st.session_state.model.predict(input_features)[0]
-        # Store the prediction in session state
-        st.session_state.predicted_price = np.expm1(predicted_price)  # Use exponential transformation if needed
-    except Exception as e:
-        st.error(f"An error occurred during prediction: {e}")
+# Button and prediction result in col2
+with col2:
+    st.header("Prediction")
+    # Button to trigger prediction
+    if st.button("Predict Price"):
+        # Input features array
+        input_features = np.array([[lf, la, oq, yb, yr, gla, fb, hb, bag, trag]])
 
-# Display the prediction
-if st.session_state.predicted_price is not None:
-    st.success(f"The predicted price is: ${st.session_state.predicted_price:,.2f}")
+        try:
+            # Perform prediction
+            predicted_price = st.session_state.model.predict(input_features)[0]
+            # Store the prediction in session state
+            st.session_state.predicted_price = np.expm1(predicted_price)  # Use exponential transformation if needed
+        except Exception as e:
+            st.error(f"An error occurred during prediction: {e}")
+
+    # Display the prediction result
+    if st.session_state.predicted_price is not None:
+        st.success(f"The predicted price is: ${st.session_state.predicted_price:,.2f}")
