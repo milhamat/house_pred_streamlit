181a9b3b6d74469061ca31d2cae5aebc36ffe45c
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-21 14:33:32 +0700

    add about

--- about.py
+++ about.py
@@ -28,3 +28,54 @@ with col2:
     """
     )
     
+st.header("Project Background")
+st.markdown(
+    """
+    This project is a part of my journey @Dibimbing Bootcamp. 
+    The main goal of this project is to predict the price of a house based on several features.
+    And use streamlit as a platform to deploy the model and presenting the data analytics, preprocessing and model prediction.
+    """
+)
+
+st.header("Author Background")
+st.markdown(
+    """
+    Ilham is a passionate Machine Learning enthusiast dedicated to harnessing the power of data to build intelligent solutions. 
+    Their expertise includes developing predictive models, optimizing algorithms, and applying deep learning techniques to address real-world challenges. 
+    With a knack for transforming complex data into actionable insights, they are committed to exploring cutting-edge AI technologies to drive innovation. 
+    Focused on solving problems through automation and intelligent systems, Ilham aims to make a meaningful impact across diverse industries.
+    """
+)
+
+st.header("Mini Projects Showcase")
+st.markdown(
+    """
+    1. **Campus Data Dashboard App** - May 2024
+        (National Taipei University of Business)
+        - Built a web data dashboard for National Taipei University of Business student alumni using Dash (Python).
+        - Setting the server and deploy the website on campus server.
+        - Design the User Interface of the Website
+    
+    Project Url: [here]()
+    
+    2. **Machine Learning Tools** - March 2024
+        (Personal Projects)
+        - Build web app for exploratory data visualization using R and Shiny.
+        - Build an regresion and Classification within the web app.
+        - Deploy the web app using shinyapps.io. 
+    
+    Project Url: [here](https://milhamat.shinyapps.io/ML_module/)
+    
+    3. **Enhancing Data Through Augmentation to Address Few-class Scenarios of Few-shot Learning** - December 2023
+        (Project article-National Taipei University of Business)
+        - Developing Image Enhancement Technique for class augmentation using CLAHE and binary inverse thresholding.
+        - Developing Class enrichment using Mathematic Combination function.
+        - Increase the model accuracy by 22%.
+    
+    4. **Steel Surface Defect Classification using Few-Shot Learning** - June 2023
+        (Master Thesis-Ming Chi University of Technology)
+        - Developing few shot learning models (Siamese, matching, prototypical network) for NEU (steel surface defect) image dataset.
+        - Combined k-means clustering to add more class based on the similarity of the images.
+        - Based on this combination the model improve 17% of accuracy.
+    """
+)
